c7fd0533f45a1b5302f7a01b90c388d324fadb9f
Júnior Gonçalves <jrgoncalves85@gmail.com>
2021-09-28 17:55:45 -0300

    otimização do código

--- ex8.py
+++ ex8.py
@@ -5,7 +5,7 @@ nota3 = int(input("Digite a sua 3º nota: "))
 resultado = (nota1 + nota2 + nota3)/3
 if resultado > 7:
     print("Parabéns ",nome, "!", "você foi aprovado(a)")
-if resultado ==7>5:
+if resultado < 7 and resultado > 5:
     print("Você ficou com media ", resultado, " e esta de recuperação")
-if resultado <5:
+if resultado < 5:
     print( nome, " você esta reprovado(a)")
